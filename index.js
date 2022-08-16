6d26a6250fbbec6d30392ee6cc7dbcbabaf25ebe
Sam Arita <arita.sam@student.moringaschool.com>
2022-08-16 12:38:19 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+let num1 = 62
+let num2 = 1
+let multiply = num1 * num2
+let random = Math.floor(Math.random()*10)+1;
+let num3 = 20
+let num4 = 8
+let mod = num3 % num4
+let max = Math.max(11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
